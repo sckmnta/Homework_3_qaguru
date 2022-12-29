a9b7fa913ca2bc52e84f4ac3006c5ea926b02a60
Nikita Maniakin <scksckscksckscksck@gmail.com>
2022-12-29 21:09:22 +0300

    added JUnit Examples

--- src/test/java/PracticeBoxTest.java
+++ src/test/java/PracticeBoxTest.java
@@ -5,17 +5,15 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.files.DownloadActions.click;
-import static java.awt.SystemColor.control;
 
 public class PracticeBoxTest {
     @BeforeAll
     static void beforeall() {
         Configuration.holdBrowserOpen = true;
+        //bConfiguration.browser = "opera";
         Configuration.browserSize = "1920x1080";
-       // Configuration.startMaximized = true;
+        // Configuration.startMaximized = true;
         Configuration.baseUrl = "https://demoqa.com";
     }
 @Test
@@ -24,7 +22,9 @@ public class PracticeBoxTest {
         $x("//input[@id='firstName']").setValue("Jovan");
         $x("//input[@id='lastName']").setValue("Savovich");
         $x("//input[@id='userEmail']").setValue("kgb@jovan.ru");
+    // не понял как делается следующий пункт, подсмотрел у других, прошу разъяснить:
         $x("//label[text()='Other']").click();
+        // Далее я попытался сделать все заполнение формы через setbyvalue но последний пункт не получился.
         $("[id=userNumber]").setValue("4999886645");
         $("[id=dateOfBirthInput]").click();
         $(".react-datepicker__year-select").selectOptionByValue("2001");
@@ -39,7 +39,8 @@ public class PracticeBoxTest {
     $("[id=currentAddress]").setValue("Leprosorium");
     $("[id=react-select-3-input]").setValue("Rajasthan").pressEnter();
     $("[id=react-select-4-input]").setValue("Jaipur").pressEnter();
-    $(".btn-primary").click();
+    $(".btn-primary")
+            .click();
     // Ура, а теперь выдача
     $(".table-responsive").shouldHave(text("Jovan Savovich"), text("kgb@jovan.ru"), text("Other"), text("4999886645"),
             text("18 December,2001"), text("Economics"), text("Computer Science"), text("Reading"), text("exmpl.jpg"), text("Leprosorium"),
