61e007c431ca76e8db90ec1100c8f28457611f97
Nikita Maniakin <scksckscksckscksck@gmail.com>
2022-12-29 21:11:12 +0300

    added JUnit Examples

---
+++ src/test/java/junit/JunitTest.java
@@ -0,0 +1,33 @@
+package junit;
+
+import org.junit.jupiter.api.*;
+
+public class JunitTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Method @BeforeAll");
+    };
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("Method @AfterAll");
+    }
+
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Method @Beforeeach");
+    }
+    @AfterEach
+    void beforeEach(){
+        System.out.println("Method @Aftereach");
+    }
+
+
+    @Test
+    void jUnitTest() {
+        Assertions.assertTrue(3 > 2);
+
+    }
+}
