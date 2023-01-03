845495a1d042e7fe17553b74f1006bd4c6883593
Nikita Maniakin <scksckscksckscksck@gmail.com>
2023-01-03 20:06:12 +0300

    fixed sytax, added execute js, added variables

--- src/test/java/PracticeBoxTest.java
+++ src/test/java/PracticeBoxTest.java
@@ -18,16 +18,24 @@ public class PracticeBoxTest {
     }
 @Test
     void practicefillformtest() {
+
+
+        /// strings
+        String firstName = "Jovan";
+        String lastName = "Savovich";
+        String userMail = "kgb@jovan.ru";
+        String userNumber = "4999886645";
+        String currentAddress = "Leprosorium";
+
         ///open form
         open("/automation-practice-form");
         ///fill form
-        $x("//input['#firstName']").setValue("Jovan");
-        $x("//input[@id='lastName']").setValue("Savovich");
-        $x("//input[@id='userEmail']").setValue("kgb@jovan.ru");
-    // не понял как делается следующий пункт, подсмотрел у других, прошу разъяснить:
+        $x("//input['#firstName']").setValue(firstName);
+        $x("//input[@id='lastName']").setValue(lastName);
+        $x("//input[@id='userEmail']").setValue(userMail);
         $x("//label[text()='Other']").click();
         // Далее я попытался сделать все заполнение формы через setbyvalue но последний пункт не получился.
-        $("#userNumber").setValue("4999886645");
+        $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOptionByValue("2001");
         $(".react-datepicker__month-select").selectOptionByValue("11");
@@ -38,7 +46,7 @@ public class PracticeBoxTest {
         $x("//label[text()='Reading']").click();
         $x("//input[@id='uploadPicture']").uploadFile(new File("src/test/resources/exmpl.jpg"));
     //$("#uploadPicture").uploadFile(new File("src/test/resources/exmpl.jpg")); как вариант
-    $("#currentAddress").setValue("Leprosorium");
+    $("#currentAddress").setValue(currentAddress);
     $("#react-select-3-input").setValue("Rajasthan").pressEnter();
     $("#react-select-4-input").setValue("Jaipur").pressEnter();
     /// hide overlay banners
@@ -46,8 +54,8 @@ public class PracticeBoxTest {
     executeJavaScript("$('footer').remove()");
     $(".btn-primary").click();
     /// Ура, а теперь выдача
-    $(".table-responsive").shouldHave(text("Jovan Savovich"), text("kgb@jovan.ru"), text("Other"), text("4999886645"),
-            text("18 December,2001"), text("Economics"), text("Computer Science"), text("Reading"), text("exmpl.jpg"), text("Leprosorium"),
+    $(".table-responsive").shouldHave(text(firstName), text(lastName), text(userMail), text("Other"), text(userNumber),
+            text("18 December,2001"), text("Economics"), text("Computer Science"), text("Reading"), text("exmpl.jpg"), text(currentAddress),
             text("Rajasthan Jaipur"));
 }
 
---
+++ src/test/java/TestBoxTest.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBoxTest {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920*1080";}
+    @Test
+    void demoqaTest() {
+        open("https://demoqa.com/text-box");
+        $(".main-header").shouldHave(text("Text BoX"));
+        $("#userName").setValue("Jovan");
+        $("#userEmail").setValue("jovan@jovan.ru");
+        $("#currentAddress").setValue("Srpska");
+        $("#permanentAddress").setValue("Rsska");
+        $("#submit").click();
+        $("#output").shouldBe(visible);
+
+    }
+}
